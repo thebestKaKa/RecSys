78e9f11bba998bd4d03b36d6464a839b336c7495
thebestKaKa <2429642242@qq.com>
2021-05-11 18:42:26 +0800

    修改topN bug

--- LFM.py
+++ LFM.py
@@ -9,26 +9,42 @@ import random
 import operator
 
 allItemSet = set()
+
+
 def InitAllItemSet(user_items):
+    """
+    :param user_items:
+    :return: 所有物品集合
+    """
     allItemSet.clear()
     for user, items in user_items.items():
         for i, r in items.items():
             allItemSet.add(i)
 
+
 def InitItems_Pool(items):
+    """
+    :param items:单个用户交互过的物品及其分数字典
+    :return:未交互的物品池
+    """
     interacted_items = set(items.keys())
     items_pool = list(allItemSet - interacted_items)
-#    items_pool = list(allItemSet)
+    #    items_pool = list(allItemSet)
     return items_pool
 
+
 def RandSelectNegativeSample(items):
+    """
+    :param items:单个用户交互过的物品及其分数字典
+    :return: 按正负比例1:1进行采样 返回结果
+    """
     ret = dict()
     for i in items.keys():
         ret[i] = 1
     n = 0
-    for i in range(0,len(items) * 3):
+    for i in range(0, len(items) * 3):
         items_pool = InitItems_Pool(items)
-        item = items_pool[random.randint(0,len(items_pool) - 1 )]
+        item = items_pool[random.randint(0, len(items_pool) - 1)]
         if item in ret:
             continue
         ret[item] = 0
@@ -37,63 +53,93 @@ def RandSelectNegativeSample(items):
             break
     return ret
 
-def Predict(user,item,P,Q):
+
+def Predict(user, item, P, Q):
     rate = 0
-    for f,puf in P[user].items():
+    for f, puf in P[user].items():
         qif = Q[item][f]
         rate += puf * qif
     return rate
 
 
-def InitModel(user_items,F):
+def InitModel(user_items, F):
+    """
+    :param user_items: 数据集
+    :param F: 嵌入向量维度
+    :return: 初始化用户和物品的嵌入向量
+    """
     P = dict()
     Q = dict()
     for user, items in user_items.items():
         P[user] = dict()
-        for f in range(0,F):
+        for f in range(0, F):
             P[user][f] = random.random()
-        for i,r in items.items():
+        for i, r in items.items():
             if i not in Q:
                 Q[i] = dict()
-                for f in range(0,F):
+                for f in range(0, F):
                     Q[i][f] = random.random()
-    return P,Q
+    return P, Q
 
 
-
-def LatentFactorModel(user_items, F,T, alpha, lamb):
+def LatentFactorModel(user_items, F, T, alpha, lamb):
+    """
+    :param user_items:训练集
+    :param F:嵌入向量维度
+    :param T:迭代次数
+    :param alpha:学习率
+    :param lamb:正则化参数
+    :return:用户和物品隐向量
+    """
     InitAllItemSet(user_items)
-    [P,Q] = InitModel(user_items, F)
-    for step in range(0,T):
+    [P, Q] = InitModel(user_items, F)
+    for step in range(0, T):
         for user, items in user_items.items():
             samples = RandSelectNegativeSample(items)
             for item, rui in samples.items():
-                eui = rui - Predict(user, item, P,Q)
-                for f in range(0,F):
-                    P[user][f] += alpha * (eui * Q[item][f] - \
-                        lamb * P[user][f])
-                    Q[item][f] += alpha * (eui * P[user][f] - \
-                        lamb * Q[item][f])
+                eui = rui - Predict(user, item, P, Q)
+                for f in range(0, F):
+                    P[user][f] += alpha * (eui * Q[item][f] - lamb * P[user][f])
+                    Q[item][f] += alpha * (eui * P[user][f] - lamb * Q[item][f])
         alpha *= 0.9
-    return P,Q
-        
-def Recommend(user,train,P,Q):
+    return P, Q
+
+
+def Recommend(user, train, P, Q):
+    """
+    给出某个用户的所有物品得分字典
+    :param user: 推荐用户编号
+    :param train:训练集
+    :param P:
+    :param Q:
+    :return: {物品:分数;...}
+    """
     rank = dict()
     interacted_items = train[user]
     for i in Q:
         if i in interacted_items.keys():
             continue
-        rank.setdefault(i,0)
-        for f,qif in Q[i].items():
+        rank.setdefault(i, 0)
+        for f, qif in Q[i].items():
             puf = P[user][f]
             rank[i] += puf * qif
     return rank
-    
-def Recommendation(users, train,P,Q):
+
+
+def Recommendation(users, train, P, Q):
+    """
+    推荐结果
+    :param users:
+    :param train:
+    :param P:
+    :param Q:
+    :return:{用户1:{item:rate;...};
+             用户2:{item:rate;...};
+             ...}
+    """
     result = dict()
     for user in users:
-        rank = Recommend(user,train,P,Q)
-        R = sorted(rank.items(), key = operator.itemgetter(1), \
-                   reverse = True)
+        rank = Recommend(user, train, P, Q)
+        R = sorted(rank.items(), key=operator.itemgetter(1), reverse=True)
         result[user] = R
     return result
--- mainCF.py
+++ mainCF.py
@@ -4,6 +4,7 @@ Created on Tue Jan  2 20:36:48 2018
 
 @author: lanlandetian
 """
+import sys
 
 import UserCF
 import UserCF_IIF
@@ -13,93 +14,94 @@ import random
 import Evaluation
 import LFM
 
-import imp
-imp.reload(UserCF)
-imp.reload(ItemCF)
-imp.reload(ItemCF_IUF)
-imp.reload(Evaluation)
-imp.reload(LFM)
+
+# import imp
+# imp.reload(UserCF)
+# imp.reload(ItemCF)
+# imp.reload(ItemCF_IUF)
+# imp.reload(Evaluation)
+# imp.reload(LFM)
 
 
 def readData():
     data = []
     fileName = './u.data'
-    fr = open(fileName,'r')
+    fr = open(fileName, 'r')
     for line in fr.readlines():
         lineArr = line.strip().split()
         data.append([lineArr[0], lineArr[1], 1.0])
     return data
-    
-    
-def SplitData(data,M,k,seed):
+
+
+def SplitData(data, M, k, seed):
     test = []
     train = []
     random.seed(seed)
-    for user, item,rating in data:
-        if random.randint(0,M-1) == k:
-            test.append([user,item,rating])
+    for user, item, rating in data:
+        if random.randint(0, M - 1) == k:
+            test.append([user, item, rating])
         else:
-            train.append([user, item,rating])
+            train.append([user, item, rating])
     return train, test
-        
-    
+
+
 # 将列表形式数据转换为dict形式
 def transform(oriData):
     ret = dict()
-    for user,item,rating in oriData:
+    for user, item, rating in oriData:
         if user not in ret:
             ret[user] = dict()
         ret[user][item] = rating
     return ret
-    
+
+
 if __name__ == '__main__':
     data = readData()
-    numFlod = 5
-    precision =0
+    numFlod = 5  #
+    precision = 0
     recall = 0
     coverage = 0
-    popularity =0
-    for i in range(0,numFlod):
-        [oriTrain,oriTest] = SplitData(data,numFlod,i,0)
+    popularity = 0
+    for i in range(0, numFlod):
+        [oriTrain, oriTest] = SplitData(data, numFlod, i, 0)
         train = transform(oriTrain)
         test = transform(oriTest)
-        
-#        W = UserCF.UserSimilarity(train)
-    #    rank = UserCF.Recommend('1',train,W)
-#        result = UserCF.Recommendation(test.keys(), train, W)
-    
-        W = UserCF_IIF.UserSimilarity(train)
-    #    rank = UserCF_IIF.Recommend('1',train,W)
-        result = UserCF_IIF.Recommendation(test.keys(), train, W)
-        
-    #    W = ItemCF.ItemSimilarity(train)
-    #    rank = ItemCF.Recommend('1',train,W)
-#        result =  ItemCF_IUF.Recommendation(test.keys(),train, W)
-        
-#        W = ItemCF_IUF.ItemSimilarity(train)
-    #    rank = ItemCF_IUF.Recommend('1',train,W)
-#        result =  ItemCF_IUF.Recommendation(test.keys(),train, W)
 
-#        [P,Q] = LFM.LatentFactorModel(train, 10,30, 0.02, 0.01)
-#        rank = LFM.Recommend('2',train,P,Q)
-#        result = LFM.Recommendation(test.keys(), train,P,Q)
+        #        W = UserCF.UserSimilarity(train)
+        #        rank = UserCF.Recommend('1',train,W)
+        #        result = UserCF.Recommendation(test.keys(), train, W)
 
+        # W = UserCF_IIF.UserSimilarity(train)
+        # rank = UserCF_IIF.Recommend('1',train,W)
+        # result = UserCF_IIF.Recommendation(test.keys(), train, W)
+
+        #    W = ItemCF.ItemSimilarity(train)
+        #    rank = ItemCF.Recommend('1',train,W)
+        #        result =  ItemCF_IUF.Recommendation(test.keys(),train, W)
+
+        #        W = ItemCF_IUF.ItemSimilarity(train)
+        #    rank = ItemCF_IUF.Recommend('1',train,W)
+        #        result =  ItemCF_IUF.Recommendation(test.keys(),train, W)
+
+        [P, Q] = LFM.LatentFactorModel(train, 10, 30, 0.02, 0.01)
+        rank = LFM.Recommend('2', train, P, Q)
+        result = LFM.Recommendation(test.keys(), train, P, Q)
 
         N = 10
-        precision += Evaluation.Precision(train,test, result,N)
-        recall += Evaluation.Recall(train,test,result,N)
-        coverage += Evaluation.Coverage(train, test, result,N)
-        popularity += Evaluation.Popularity(train, test, result,N)
-       
+        precision += Evaluation.Precision(train, test, result, N)
+        recall += Evaluation.Recall(train, test, result, N)
+        coverage += Evaluation.Coverage(train, test, result, N)
+        popularity += Evaluation.Popularity(train, test, result, N)
+        print(i)
+
+
     precision /= numFlod
     recall /= numFlod
     coverage /= numFlod
     popularity /= numFlod
-    
-    
-     #输出结果
-    print('precision = %f' %precision)
-    print('recall = %f' %recall)
-    print('coverage = %f' %coverage)
-    print('popularity = %f' %popularity)
-    
+
+    # 输出结果
+    print('precision = %f' % precision)
+    print('recall = %f' % recall)
+    print('coverage = %f' % coverage)
+    print('popularity = %f' % popularity)
